83f2e0fee22c03398060c00cf59850a9862ceec4
Nolann <nolann.juet@gmail.com>
2022-02-04 18:54:19 +0100

    Vector : length, angle, From<[T; 3]>, Display, Clone

--- src/math/vector.rs
+++ src/math/vector.rs
@@ -1,4 +1,5 @@
 use std::ops::{Add, Mul};
+use std::fmt::Display;
 
 pub struct Vec3 {
     pub x: f64,
@@ -16,8 +17,16 @@ impl Vec3 {
         }
     }
 
+    pub fn length(&self) -> f64 {
+        (self.x.powi(2) + self.y.powi(2) + self.z.powi(2)).sqrt()
+    }
+
     pub fn scalar(&self, vector: &Self) -> f64 {
         self.x * vector.x + self.y * vector.y + self.z * vector.z
+    }
+
+    pub fn angle(&self, vector: &Self) -> f64 {
+        ((self.scalar(vector)/(self.length() * vector.length()))).acos()
     }
 }
 
@@ -42,6 +51,32 @@ impl Add for &Vec3 {
             x: self.x + rhs.x,
             y: self.y + rhs.y,
             z: self.z + rhs.z,
+        }
+    }
+}
+
+impl<T: Into<f64> + Copy> From<[T; 3]> for Vec3 {
+    fn from(list: [T; 3]) -> Self {
+        Self {
+            x: list[0].into(),
+            y: list[1].into(),
+            z: list[2].into(),
+        }
+    }
+}
+
+impl Display for &Vec3 {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "({}, {}, {})", self.x, self.y, self.z)
+    }
+}
+
+impl Clone for Vec3 {
+    fn clone(&self) -> Self {
+        Self {
+            x: self.x,
+            y: self.y,
+            z: self.z,
         }
     }
 }
