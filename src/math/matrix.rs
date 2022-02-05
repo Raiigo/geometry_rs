eae2e60fa28756ad27fc85ee5acb2bc0d84dbadd
Nolann <nolann.juet@gmail.com>
2022-02-05 19:07:15 +0100

    Matrix : identity

--- src/math/matrix.rs
+++ src/math/matrix.rs
@@ -18,6 +18,31 @@ impl<const ROW: usize, const COLUMN: usize> Matrix<ROW, COLUMN> {
             content: [[0.0; ROW]; COLUMN],
         }
     }
+    pub fn identity() -> Self {
+        let content_vec: Vec<Vec<f64>> = [[0.0; ROW]; COLUMN].iter_mut().enumerate().map(|(j, l)| {
+            l.into_iter().enumerate().map(|(i, e)| {
+                if i == j {
+                    1.0
+                } else {
+                    *e
+                }
+            }).collect()
+        }).collect();
+
+        let mut content_array: [[f64; ROW]; COLUMN] = [[0.0; ROW]; COLUMN];
+
+        for i in 0..ROW {
+            for j in 0..COLUMN {
+                content_array[j][i] = content_vec[j][i];
+            }
+        }
+
+        Self {
+            row: ROW,
+            column: COLUMN,
+            content: content_array,
+        }
+    }
 
     pub fn get_element<T: Into<usize> + Copy>(&self, i: T, j: T) -> Option<f64> {
         if i.into() > Self::ROW || j.into() > Self::COLUMN {
