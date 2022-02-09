faf8ba3a5c2ebdea8e57f150967c67adc3dc0961
Nolann <nolann.juet@gmail.com>
2022-02-09 19:20:50 +0100

    Vector : Added VecD struct, a D dimension vector, removed Vec3, all old Vec3 method works with VecD

--- src/math/vector.rs
+++ src/math/vector.rs
@@ -1,27 +1,44 @@
 use std::fmt::Display;
 use std::ops::{Add, Mul};
 
-pub struct Vec3 {
-    pub x: f64,
-    pub y: f64,
-    pub z: f64,
+pub struct VecD<const SIZE: usize> {
+    pub size: usize,
+    content: [f64; SIZE],
 }
 
-impl Vec3 {
-    pub fn new<T: Into<f64>>(x: T, y: T, z: T) -> Self {
+impl<const SIZE: usize> VecD<SIZE> {
+
+    pub fn new() -> Self {
         Self {
-            x: x.into(),
-            y: y.into(),
-            z: z.into(),
+            size: SIZE,
+            content: [0.0; SIZE],
+        }
+    }
+
+    pub fn get_element<T: Into<usize> + Copy>(&self, i: T) -> Option<f64> {
+        if i.into() < SIZE {
+            Option::Some(self.content[i.into()])
+        } else {
+            Option::None
+        }
+    }
+
+    pub fn get_mut_element<T: Into<usize> + Copy>(&mut self, i: T) -> Option<&mut f64> {
+        if i.into() < SIZE {
+            Option::Some(&mut self.content[i.into()])
+        } else {
+            Option::None
         }
     }
 
     pub fn length(&self) -> f64 {
-        (self.x.powi(2) + self.y.powi(2) + self.z.powi(2)).sqrt()
+        self.content.into_iter().sum::<f64>().sqrt()
     }
 
     pub fn scalar(&self, vector: &Self) -> f64 {
-        self.x * vector.x + self.y * vector.y + self.z * vector.z
+        self.content.into_iter().enumerate().map(|(i, e)| {
+            e * vector.content[i]
+        }).collect::<Vec<f64>>().into_iter().sum::<f64>()
     }
 
     pub fn angle(&self, vector: &Self) -> f64 {
@@ -29,52 +46,76 @@ impl Vec3 {
     }
 }
 
-impl<T: Into<f64> + Copy> Mul<T> for &Vec3 {
-    type Output = Vec3;
+impl<T: Into<f64> + Copy, const SIZE: usize> Mul<T> for &VecD<SIZE> {
+    type Output = VecD::<SIZE>;
 
     fn mul(self, rhs: T) -> Self::Output {
-        Vec3 {
-            x: self.x * rhs.into(),
-            y: self.y * rhs.into(),
-            z: self.z * rhs.into(),
+        let content_vec = self.content.clone().into_iter().map(|e| {
+            e * rhs.into()
+        }).collect::<Vec<f64>>();
+        let mut content = [0.0; SIZE];
+        for (i, e) in content_vec.into_iter().enumerate() {
+            content[i] = e;
+        }
+        
+        VecD::<SIZE> {
+            size: SIZE,
+            content: content,
         }
     }
 }
 
-impl Add for &Vec3 {
-    type Output = Vec3;
+impl<const SIZE: usize> Add for &VecD<SIZE> {
+    type Output = VecD::<SIZE>;
 
     fn add(self, rhs: Self) -> Self::Output {
-        Vec3 {
-            x: self.x + rhs.x,
-            y: self.y + rhs.y,
-            z: self.z + rhs.z,
+        let content_vec: Vec<f64> = self.content.clone().into_iter().enumerate().map(|(i, e)| {
+            e * rhs.content[i]
+        }).collect::<Vec<f64>>();
+        let mut content = [0.0; SIZE];
+        for (i, e) in content_vec.into_iter().enumerate() {
+            content[i] = e;
+        }
+
+        VecD::<SIZE> {
+            size: SIZE,
+            content: content,
         }
     }
 }
 
-impl<T: Into<f64> + Copy> From<[T; 3]> for Vec3 {
-    fn from(list: [T; 3]) -> Self {
+impl<T: Into<f64> + Copy, const SIZE: usize> From<[T; SIZE]> for VecD<SIZE> {
+    fn from(list: [T; SIZE]) -> Self {
+        let mut content = [0.0; SIZE];
+        for (i, e) in list.into_iter().enumerate() {
+            content[i] = e.into();
+        }
+        
         Self {
-            x: list[0].into(),
-            y: list[1].into(),
-            z: list[2].into(),
+            size: SIZE,
+            content: content,
         }
     }
 }
 
-impl Display for Vec3 {
+impl<const SIZE: usize> Display for VecD<SIZE> {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(f, "({}, {}, {})", self.x, self.y, self.z)
+        let mut line: String = "(".into();
+        for e in self.content {
+            line.push_str(&format!("{}, ", e));
+        }
+        line.remove(line.len() - 1);
+        line.remove(line.len() - 2);
+        line.push(')');
+        write!(f, "{}", line)
     }
 }
 
-impl Clone for Vec3 {
+impl<const SIZE: usize> Clone for VecD<SIZE> {
     fn clone(&self) -> Self {
         Self {
-            x: self.x,
-            y: self.y,
-            z: self.z,
+            size: SIZE,
+            content: self.content.clone(),
         }
     }
 }
